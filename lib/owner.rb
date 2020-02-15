4e0dd830f495e183ef5cb4abcbeb3475b51e2eef
spensation <spencevans@gmail.com>
2020-02-15 03:11:35 -0500

    Owner tests passing

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+	attr_accessor 
+	attr_reader :name, :species
+  	@@all = []
+
+  	def initialize(name)
+  		@name = name
+  		@species = "human"
+  		save
+  	end
+
+  	def save
+  		@@all << self
+  	end
+
+  	def say_species
+  		"I am a #{species}."
+  	end
+
+  	def self.all 
+  		@@all
+  	end
+
+  	def self.count
+  		@@all.size
+  	end
+
+  	def self.reset_all
+  		@@all = []
+  	end
 end
